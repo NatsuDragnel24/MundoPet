fa3b1cd7cc08b19607b3b71bd818b14358e6a6a6
NatsuDragnel24 <brahiam.bl1999@hotmail.es>
2025-04-01 13:41:33 -0500

    Creación del Footer y Header - Configuración adicional para los accesos de cada archivo y carpetas nuevas.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+      RouterOutlet,
+      HeaderComponent,
+      FooterComponent
+    ],
+  standalone: true, // Marca este componente como independiente
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home/home.component';
+//import { ContactComponent } from './components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redirección al Home
+  { path: 'home', component: HomeComponent }         // Ruta para la página principal
+  //,{ path: 'contact', component: ContactComponent }    // Ruta para la página de contacto
+];
